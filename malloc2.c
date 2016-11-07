8c7ac5bb40146f446209b418f63b04c0760e1bec
Fausto G. Tommasi <mmasito123@yahoo.com>
2016-11-07 15:44:55 -0600

    Rewrite of malloc2.c complete (malloc.c now deprecated)

--- malloc2.c
+++ malloc2.c
@@ -12,18 +12,18 @@ struct node{
   char free;
 }node;
 
-#define node struct node
 
-node head_node = {
+struct node head_node = {
   .next = NULL,
   .addr = 0,
   .size = 0,
   .free = 0,
 };
-node* head = & head_node;
+
+struct node* head = & head_node;
 
 size_t round(unsigned long to_round, unsigned int factor){
-  return (size_t)to_round + (factor-to_round % factor);
+  return (size_t)(to_round + (factor-to_round % factor));
 }
 
 size_t round_to_page_size(unsigned long to_round){
@@ -35,18 +35,26 @@ size_t round_to_eight(unsigned long to_round){
 }
 
 
-void split_node(node* current, size_t requested_size){
-  node* second = ((char*)current->addr + requested_size);
-  
+void split_node(struct node* current, size_t requested_size){
+  struct node* second = ((char*)current->addr + requested_size);
+  second->size = current->size -(requested_size + sizeof(struct node));
+  second->addr = round_to_eight((char*)second + sizeof(struct node));
+  second->free = 1;
+  second->next = current->next;
+
+  current->size = requested_size;
+  current->next = second;
+  current->free = 0;
 }
 
 void* TEST_malloc(size_t u_size){
-  node* curr = head;
+  if(u_size < 1)
+    return NULL;
+  struct node* curr = head;
   while(curr->next){
-    curr = curr->next;
     if(curr->free){
      
-     if(curr->size > (u_size + 2*sizeof(node))){
+     if(curr->size > (u_size + 2*sizeof(struct node))){
        split_node(curr,u_size); 
        return curr->addr;
      } 
@@ -54,34 +62,34 @@ void* TEST_malloc(size_t u_size){
        curr->free = 0;
        return curr->addr;
      }
-
-
     
     }
-
+    curr = curr->next;
   }
-  void* new = sbrk(round_to_page_size(u_size + sizeof(node)));
+  void* new = sbrk(round_to_page_size(u_size + sizeof(struct node)));
   if(new == (void*)-1){
     errno = ENOMEM;
     exit(-1);
   }
   
-  node* allocated;
-  node* remaining;
+ struct  node* allocated;
+ struct  node* remaining;
 
-  allocated = (node*) new;
-  allocated->addr = (void*)round_to_eight((char*)allocated + sizeof(node));
+  allocated = (struct node*) new;
+  allocated->addr = (void*)round_to_eight((char*)allocated + sizeof(struct node));
   allocated->next = NULL;
   allocated->free = 0;
   allocated->size = u_size;
 
   curr->next = allocated;
-  int space_left = 0;
-  if(space_left > sizeof(node)){
-    remaining = (node*) ((char*)allocated->addr + u_size);
-    remaining->addr = round_to_eight((char*)remaining + sizeof(node));
+  long space_left = 
+    round_to_page_size(u_size + sizeof(struct node)) 
+    - u_size + 2* sizeof(struct node);
+  if(space_left > sizeof(struct node)){
+    remaining = (struct node*) ((char*)allocated->addr + u_size);
+    remaining->addr = round_to_eight((char*)remaining + sizeof(struct node));
     remaining->free = 1;
-    remaining->size = space_left - sizeof(node);
+    remaining->size = space_left - sizeof(struct node);
     remaining->next = NULL;
 
     allocated->next = remaining;
@@ -91,17 +99,91 @@ void* TEST_malloc(size_t u_size){
 }
 
 void TEST_free(void* addr){
-  node* curr = head;
+  struct node* curr = head;
   while(curr->next && curr->addr != addr){
     curr = curr->next;
   }
 
   if(curr->addr == addr){
-    curr->free = 0;
+    curr->free = 1;
   }
 }
 // --------- CHURN ------------------
+#define NUMSLOTS 40
+
+char *slots[NUMSLOTS];
+size_t sizes[NUMSLOTS];
+
 int churn_main(int argc, char** argv){
+
+int i,n,size,maxblock;
+  long j;
+  int verbose=0;
+
+  // Read arguments
+  maxblock = -1;
+  if (argc == 3) {
+    if (!strcmp(argv[1],"-d")) {
+      maxblock = atoi(argv[2]);
+      verbose = 1;
+    }
+  }
+  if (argc == 2)
+    maxblock = atoi(argv[1]);
+
+  if (maxblock <= 0) {
+    fprintf(stderr,"usage: churn [-d] maxblock\n");
+    exit(1);
+  }
+
+  // Initialize
+  for (i = 0; i<NUMSLOTS; i++) slots[i] = NULL;
+  srand(time(NULL));
+
+  long mallocs=0,frees=0;  // number of mallocs, number of frees
+  printf("mallocs\tfrees\tsbrk\n");
+
+  // Main loop:
+  while (1) {
+    n = rand() % NUMSLOTS;
+    
+    if (slots[n]) {
+      // Going to free slot[n]
+      // Check contents to see if it's still 0,1,2,3,4,...
+      for (j=1; j<sizes[n]; j++) {
+	if (*(slots[n]+j) != (char) j) {
+	  printf("Memory corruption detected.\n");
+	  exit(1);
+	}
+      }
+      
+      if (verbose) printf("freeing slot %d.\n",n);
+      TEST_free(slots[n]);
+      slots[n] = NULL;
+      frees++;
+
+    } else {
+      // Going to malloc slot[n]
+      if (verbose) printf("malloc slot %d.\n",n);
+
+      sizes[n] = rand() % maxblock + 1;
+      slots[n] = (char *) TEST_malloc (sizes[n]);
+      if (slots[n] == NULL) {
+	fprintf(stderr,"out of memory\n");
+	exit(1);
+      }
+      
+      // Fill block with 0,1,2,3,4,...
+      for (j=0; j<sizes[n]; j++) *(slots[n]+j) = (char) j;
+
+      mallocs++;
+    }
+    
+    // Periodically report progress
+    if ((mallocs + frees) % 1000 == 0) 
+      printf("%ld\t%ld\t%p\n",mallocs,frees,sbrk(0));
+  }
+
 
 }
 
@@ -117,3 +199,5 @@ int main(int argc, char** argv){
   churn_main(argc,argv);
   //churn_2_main(argc,argv);
 }
+
+  
